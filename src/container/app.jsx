abea794cb6fc347bce81011610da0dbb640e5602
Omer Prizner <omerp@wix.com>
2017-10-27 20:09:42 +0300

    Initial Commit

--- src/container/app.jsx
+++ src/container/app.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from 'src/component/header.jsx';
 
 export default class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div>
-          <Header />
-          Your app
-        </div>
-      </MuiThemeProvider>
+      <div>
+        <Header />
+        Your app
+      </div>
     );
   }
 }
